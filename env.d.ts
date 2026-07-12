9f083e4aeedb5088c65ce10ebdabbfdc397d24f5
alaltitov <alaltitov@gmail.com>
2026-07-12 16:27:35 +0300

    fix langs, fix eth for jc1060p70

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+
+declare module 'yandex-metrika-vue3';
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import i18n from './plugins/i18n'
+import yandexMetrika from 'yandex-metrika-vue3'
 
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
@@ -15,5 +16,17 @@ const app = createApp(App)
 app.use(router)
 app.use(vuetify)
 app.use(i18n)
+
+app.use(yandexMetrika, {
+  id: 110625024,
+  router: router,
+  env: import.meta.env.MODE,
+  options: {
+    clickmap: true,
+    trackLinks: true,
+    accurateTrackBounce: true,
+    webvisor: true
+  }
+})
 
 app.mount('#app')
