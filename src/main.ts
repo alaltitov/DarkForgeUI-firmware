1d757c91230eeb7878bc75737c1ece93734c0910
alaltitov <alaltitov@gmail.com>
2026-07-12 16:34:24 +0300

    fix metrika

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import i18n from './plugins/i18n'
-import { yandexMetrika } from 'yandex-metrika-vue3'
+import { initYandexMetrika } from 'yandex-metrika-vue3'
 
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
@@ -17,7 +17,7 @@ app.use(router)
 app.use(vuetify)
 app.use(i18n)
 
-app.use(yandexMetrika, {
+app.use(initYandexMetrika, {
   id: 110625024,
   router: router,
   env: import.meta.env.MODE,
